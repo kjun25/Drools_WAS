28105cfd6a2f9a4b12c3ea18316b09daa62c9dcb
kyungjun Yun <sound605@gmail.com>
2017-12-13 16:28:51 +0900

    first commit

---
+++ drools_WAS/src/main/java/com/kjun/drools/CheckingMyCar.java
@@ -0,0 +1,17 @@
+package com.kjun.drools;
+
+public class CheckingMyCar {
+	private int carnum;
+	
+	 public CheckingMyCar(int carnum){
+	      this. carnum = carnum;	     
+	    }
+	public int getCarnum() {
+		return carnum;
+	}
+
+	public void setCarnum(int carnum) {
+		this.carnum = carnum;
+	}
+	
+}
---
+++ drools_WAS/src/main/java/com/kjun/drools/Drools_WAS.java
@@ -0,0 +1,78 @@
+package com.kjun.drools;
+
+import java.util.Iterator;
+
+import org.kie.api.KieBase;
+import org.kie.api.definition.KiePackage;
+import org.kie.api.definition.rule.Rule;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
+
+public class Drools_WAS {
+    public static final void main(String[] args) {
+
+	try {
+
+		// create new KnowledgeBuilder 
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+
+		// 지식 빌더에 룰정의파일 설정
+		kbuilder.add(ResourceFactory.newClassPathResource("number_of_car.drl"), ResourceType.DRL);
+
+		// 새로운 지식 베이스 생성
+		KieBase kiebase = kbuilder.newKnowledgeBase();
+
+		Iterator<KiePackage> lKiePackageIterator = kiebase.getKiePackages().iterator();
+		while (lKiePackageIterator.hasNext()) {
+			KiePackage lKiePackage = lKiePackageIterator.next();
+			Iterator<Rule> lRuleIterator = lKiePackage.getRules().iterator();
+			while (lRuleIterator.hasNext()) {
+				Rule lRule = lRuleIterator.next();
+				System.out.println(lRule.getName());
+			}
+		}
+
+		// 새로운 지식 베이스 세션 생성
+	KieSession session = kiebase.newKieSession();
+
+		System.out.println(session.getSessionClock().getCurrentTime());
+
+		// logger등록(팩트에 의해 생성된 엑티베이션(Activation)관련 이벤트만 로깅
+//		session.addEventListener(new CustomAgendaEventListener(session.getIdentifier()));
+
+		// CheckingMyCar클래스를 이용한 팩트 추가
+		int a = 0;
+		
+			session.insert(new CheckingMyCar(a));
+		
+
+			System.out.println(session.getSessionClock().getCurrentTime());
+
+			 System.out.println(session.getIdentifier());
+
+			System.out.println("RuleEngineStartTime : " + session.getSessionClock().getCurrentTime()
+					+ " Matched count of Rule : " + session.fireAllRules());
+
+		
+		session.dispose();
+
+//		File aFile = new File("src/main/resources");
+//		File[] fileList = aFile.listFiles();
+//
+//		for (File tempFile : fileList) {
+//			if (tempFile.isFile()) {
+//				String tempPath = tempFile.getParent();
+//				String tempFileName = tempFile.getName();
+//				System.out.println(tempFileName);
+//			}
+//		}
+
+	} catch (Throwable t) {
+		t.printStackTrace();
+	}
+}
+
+}
